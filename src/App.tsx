5ab02c20b33bc16421cbf73ec386fec86d29ca9e
Gogo200202 <gosheto420@gmail.com>
2025-11-24 15:51:18 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+//import { coreURL } from "@ffmpeg/core?url";
+// import wasmURL from "@ffmpeg/core/wasm?url";
+
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile, toBlobURL } from "@ffmpeg/util";
+import { useRef, useState } from "react";
+import VideoInput from "./VideoInput";
+
+export function App() {
+  const [loaded, setLoaded] = useState(false);
+  const [blobUrl, setbloburl] = useState("");
+  const ffmpegRef = useRef(new FFmpeg());
+
+  const videoRef: any = useRef(null);
+  const messageRef: any = useRef(null);
+
+  function GetblobUrl(url: string) {
+    setbloburl(url);
+  }
+
+  const load = async () => {
+    const baseURL =
+      "https://cdn.jsdelivr.net/npm/@ffmpeg/core@0.12.10/dist/esm";
+    const ffmpeg = ffmpegRef.current;
+
+    ffmpeg.on("log", ({ message }) => {
+      messageRef.current.innerHTML = message;
+      console.log(message);
+    });
+    // toBlobURL is used to bypass CORS issue, urls with the same
+    // domain can be used directly.
+    await ffmpeg.load({
+      coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, "text/javascript"),
+      wasmURL: await toBlobURL(
+        `${baseURL}/ffmpeg-core.wasm`,
+        "application/wasm"
+      ),
+    });
+
+    setLoaded(true);
+  };
+
+  const transcode = async () => {
+    const ffmpeg = ffmpegRef.current;
+    await ffmpeg.writeFile("input.mp4", await fetchFile(blobUrl));
+
+    //-i input.mp4 -ss 00:05:10 -to 00:15:30 -c:v copy -c:a copy output2.mp4
+
+    //"-i", inputFileName, "-ss", `${minTime}`, "-to", `${maxTime}`, "-f", "gif", outputFileName
+    await ffmpeg.exec([
+      "-ss",
+      "00:01:00",
+      "-to",
+      "00:02:00",
+      "-i",
+      "input.mp4",
+      "-c",
+      "copy",
+      "output.mp4",
+    ]);
+
+    //await ffmpeg.exec(["-i", "input.mp4", "output.webm"]);
+    const data = await ffmpeg.readFile("output.mp4");
+
+    videoRef.current.src = URL.createObjectURL(
+      new Blob([data.buffer], { type: "video/mp4" })
+    );
+  };
+
+  return loaded ? (
+    <>
+      <div className="App">
+        <h1>Video upload</h1>
+        <VideoInput GetblobUrl={GetblobUrl} />
+      </div>
+      <video ref={videoRef} controls></video>
+      <br />
+      <button onClick={transcode}>cut video</button>
+      <p ref={messageRef}></p>
+      <p>Open Developer Tools (Ctrl+Shift+I) to View Logs</p>
+    </>
+  ) : (
+    <button onClick={load}>Load ffmpeg-core (~31 MB)</button>
+  );
+}
+
+export default App;
---
+++ src/VideoInput.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+
+export default function VideoInput(props: any) {
+  const {GetblobUrl } = props;
+
+  const inputRef: any = useRef("");
+
+
+
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    GetblobUrl(url);
+    
+  };
+
+  const handleChoose = (event: any) => {
+    inputRef.current.click();
+  };
+
+  return (
+    <div className="VideoInput">
+      <input
+        ref={inputRef}
+        className="VideoInput_input"
+        type="file"
+        onChange={handleFileChange}
+        accept=".mov,.mp4"
+      />
+     
+    </div>
+  );
+}
